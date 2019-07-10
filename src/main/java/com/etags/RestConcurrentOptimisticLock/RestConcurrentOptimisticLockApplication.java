32612d33616249cbb1b74e50c9ff89267ed6010e
Paulo Machado <pauloalexandre3d@gmail.com>
2019-07-10 16:46:50 +0200

    first commit

---
+++ src/main/java/com/etags/RestConcurrentOptimisticLock/RestConcurrentOptimisticLockApplication.java
@@ -0,0 +1,13 @@
+package com.etags.RestConcurrentOptimisticLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConcurrentOptimisticLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestConcurrentOptimisticLockApplication.class, args);
+	}
+
+}
